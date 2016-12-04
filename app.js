ceb5bca3d39dea3096d9c08ca2ebff0c80a5b0c8
Angel Eduardo Mata <angelmata9313@hotmail.com>
2016-12-04 01:47:53 -0600

    Nav bar

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express();
 
+app.set("view engine","jade");
+
+app.use(express.static("recursos"));
+
 app.get("/",function(req,res){
-	res.send("hola mundo");
+	//res.send("hola mundo");
+	res.render("index");
 });
 
 app.listen(8080);
